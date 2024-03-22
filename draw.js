cef6fdd07dacc8e306825cf27c6081a4e5265df0
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2024-03-22 18:51:18 +0900

    リファクタリング

--- draw.js
+++ draw.js
@@ -1,5 +1,8 @@
+let destData = {};
+
 const _select = (v) => {
 	const visibleNonExist = true;
+	const diff = (v.diff || false) ? `${v.x},${v.y};${v.sideX},${v.sideY}` : `${v.x},${v.y}`;
 
 	if (v.neverDisplay) {
 		return;
@@ -8,27 +11,25 @@ const _select = (v) => {
 		return `<hr>`
 	}
 	if (v.nonExist && visibleNonExist) {
-		return `<option value="${v.x},${v.y}">${v.name}</option>`
+		return `<option value="${diff}">${v.name}</option>`
 	}
 	if (!v.disabled) {
-		return `<option value="${v.x},${v.y}">${v.name}</option>`
+		return `<option value="${diff}">${v.name}</option>`
 	} else {
-		return `<option value="${v.x},${v.y}" disabled>${v.name}</option>`
+		return `<option value="${diff}" disabled>${v.name}</option>`
 	}
 };
 
 async function drawSelectSeishin() {
-	const data = await (await fetch("data/dest.json")).json();
-
-	let str = data.seishin.map(_select).join("\n");			
+	let str = destData.seishin.map(_select).join("\n");			
 	document.querySelector("#select-dest").innerHTML = str;
 }
 async function drawSelectKaigan() {
-	const data = await (await fetch("data/dest.json")).json();
-	let str = data.kaigan.map(_select).join("\n");			
+	let str = destData.kaigan.map(_select).join("\n");			
 	document.querySelector("#select-dest").innerHTML = str;
 }
 (async () => {
+	destData = await (await fetch("data/dest.json")).json();
 	const data = ["_", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 	let str = data.map((v, i) => `<option value="${i}">${v}</option>`).join("\n");
 	document.querySelector("#select-number1").innerHTML = str;
---
+++ script.js
@@ -0,0 +1,51 @@
+const SCALE = 5;
+const number1Led = new LedDisplay("number1-led", "./6000LED.png", 12, 32, 0, 0, SCALE);
+const number2Led = new LedDisplay("number2-led", "./6000LED.png", 12, 32, 0, 0, SCALE);
+/* const _spaceLed = */ new LedDisplay("front-led-space", "./6000LED.png", 8, 32, 0, 0, SCALE);
+const destLed = new LedDisplay("dest-led", "./6000LED.png", 96, 32, 12, 0, SCALE);
+const destSideLed = new LedDisplay("dest-led-side", "./6000LED.png", 96, 32, 12, 0, SCALE);
+
+const destOnchange = () => {
+	const rawValue = String(document.querySelector("#select-dest").value);
+	if (rawValue.includes(";")) {
+		let value = document.querySelector("#select-dest").value.split(";").flatMap(v => v.split(","));
+		destLed.update(value[0], value[1]);
+		destSideLed.update(value[2], value[3]);
+	} else {
+		let value = document.querySelector("#select-dest").value.split(",");
+		destLed.update(value[0], value[1]);
+		destSideLed.update(value[0], value[1]);
+	}
+};
+const n1Onchange = () => {
+	let value = document.querySelector("#select-number1").value;
+	number1Led.update(0, value);
+}
+const n2Onchange = () => {
+	let value = document.querySelector("#select-number2").value;
+	number2Led.update(0, value);
+}
+document.querySelector("#select-dest").onchange = destOnchange;
+document.querySelector("#select-number1").onchange = n1Onchange;
+document.querySelector("#select-number2").onchange = n2Onchange;
+document.querySelector("#line-changer").onchange = () => {
+	const line = document.getElementById("radio-seishin").checked === true ? 0 : 1;
+	if (line === 0) {
+		drawSelectSeishin();
+		document.querySelector(".side-led").classList.remove("type5000");
+		document.querySelector(".side-led").classList.add("type6000");
+		[number1Led, number2Led, destLed, destSideLed]
+			.forEach(v => v.changeImage("./6000LED.png"));
+	} else if (line === 1) {
+		drawSelectKaigan();
+		document.querySelector(".side-led").classList.remove("type6000");
+		document.querySelector(".side-led").classList.add("type5000");
+		[number1Led, number2Led, destLed, destSideLed]
+			.forEach(v => v.changeImage("./5000LED.png"));
+	}
+	destOnchange();
+	n1Onchange();
+	n2Onchange();
+	destSideLed.update(0, 0);
+	destLed.update(0, 0);
+}
